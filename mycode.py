c4b8006f000b1bfc0319a3d0bf770f5ea463ea80
Rana <ranaalihaider2606@gmail.com>
2025-09-27 15:07:47 +0500

    second version of data saved

--- mycode.py
+++ mycode.py
@@ -5,14 +5,14 @@ import os
 data = {
     "Name": ['Alice', 'Bob', 'Charlie'],
     'Age': [25, 30, 35],
-    "CIty": ['New York', 'Los Angeles', 'Chicago']
+    "City": ['New York', 'Los Angeles', 'Chicago']
 }
 
 df = pd.DataFrame(data)
 
 # Adding new row to df for V2
-# new_row_loc = {"Name": 'V2', 'Age':20, 'City': 'City1'}
-# df.loc[len(df.index)] = new_row_loc
+new_row_loc = {"Name": 'GF1', 'Age':20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 # Adding new row to df for V3
 # new_row_loc2 = {"Name": 'V3', 'Age':30, 'City': 'City1'}
